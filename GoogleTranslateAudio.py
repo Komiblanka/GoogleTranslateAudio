13d257f81fb5752434fffd9ccd8ed54e16559e32
Komiblanka <ahervalejo@gmail.com>
2014-11-11 21:11:03 +0100

    Renamed variables to english

--- GoogleTranslateAudio.py
+++ GoogleTranslateAudio.py
@@ -6,13 +6,12 @@ from sys import argv
 import time
 
 
-def text2voicefile(text, fichero): # Dado un texto, te lo guarda en audio en un fichero.
-	texturl = urllib.quote_plus(text)   
+def text2voicefile(text, text_file): # Dado un texto, te lo guarda en audio en un text_file.
+	text_url = urllib.quote_plus(text)   
 	url = "http://translate.google.com/translate_tts?tl=en&q="
-	command = "wget -q -U Mozilla -O " + fichero +  " \"" + url + texturl + "\""
-	#print command
+	command = "wget -q -U Mozilla -O " + text_file +  " \"" + url + text_url + "\""
 	os.system(command)
-
+    
 def get_google_voice(phrase):
 	language='en' #Setting language.
 	url = "http://translate.google.com/translate_tts" #Google translate url for getting sound.
@@ -25,45 +24,41 @@ def get_google_voice(phrase):
 	response = urllib2.urlopen(request)
 	return response
 
-def findcharmax(cadena, longitudma, caracter): # busca el caracter mas a la derecha sin pasarse del longitudma
-    cont = longitudma 
-    if len(cadena) < longitudma:
+def findcharmax(text_string, max_string_length, character): # busca el character mas a la derecha sin pasarse del max_string_length
+    counter = max_string_length 
+    if len(text_string) < max_string_length:
         return -1
-    for i in reversed(cadena[0:longitudma + 1]):
-        if i == caracter:
-            return cont
-        cont -= 1
+    for i in reversed(text_string[0:max_string_length + 1]):
+        if i == character:
+            return counter
+        counter -= 1
     return -1
 
-def dividesentence(parrafo, maxlength, frases):
-    if len(parrafo) < maxlength:
-        frases.append(parrafo) # Si la frase cabe, la pone 
-        return frases
+def divide_sentence(paragraph, maxlength, phrases):
+    if len(paragraph) < maxlength:
+        phrases.append(paragraph) # Si la frase cabe, la pone 
+        return phrases
     characters = ".", ",", " "
 
     for character in characters: #go through all characters
-        position = findcharmax(parrafo, maxlength, character)
+        position = findcharmax(paragraph, maxlength, character)
         if position > -1:
-            frases.append(parrafo[0:position])
-            return dividesentence(parrafo[position + 1:len(parrafo)-1], maxlength, frases) #llamada recursiva position 
+            phrases.append(paragraph[0:position])
+            return divide_sentence(paragraph[position + 1:len(paragraph)-1], maxlength, phrases) #llamada recursiva position 
     
-    frases.append(parrafo[0:maxlength])
-    return dividesentence(parrafo[maxlength + 1:len(parrafo)-1], maxlength, frases) #llamada recursiva maxlength
+    phrases.append(paragraph[0:maxlength])
+    return divide_sentence(paragraph[maxlength + 1:len(paragraph)-1], maxlength, phrases) #llamada recursiva maxlength
 
-    
+script, text_file = argv
+text = open(text_file).read()
+paragraphs = text.split('*')
+counter = 0
 
-
-
-script, fichero = argv
-text = open(fichero).read()
-parrafos = text.split('*')
-cont = 0
-
-for i in parrafos:
-	frases = dividesentence(i, 100, [])
-	with open("Audio" + str(cont) + ".mp3", 'wb') as file:
-		for j in frases:
+for i in paragraphs:
+	phrases = divide_sentence(i, 100, [])
+	with open("Audio" + str(counter) + ".mp3", 'wb') as file:
+		for j in phrases:
 			response = get_google_voice(j)
 			file.write(response.read())
 	file.close()
-	cont += 1
+	counter += 1
